bbcd306b43e484b941eea54b2fe4375137ba379b
zhanzhechew <zhanzhechew@gmail.com>
2024-12-02 13:26:04 +0800

    asd

---
+++ _head.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $_title ?? 'Untitled' ?></title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="https://cms.cdn.91app.com.my/cms/common/iconFonts/v1.1.13/nine1/nine1.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="header-container">
+            <div class="container-left">
+                <a href="/index.php"><img src="/image/logo.png"></a>
+            </div>
+            <div class="container-right">
+                <div class="container-right1">
+                    <ul>
+                        <li class="right">
+                            <i class="ico ico-user"></i>
+                        </li>
+                        <li class="right">
+                            <i class="ico ico-shopping"></i>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-right2">
+                    <nav>
+                        <ul>
+                            <li class="left">
+                                What's Hot
+                                <i class="ico ico-chevron-down"></i>
+                            </li>
+                            <li class="left">
+                                Men
+                                <i class="ico ico-chevron-down"></i>
+                            </li>
+                            <li class="left">
+                                Women
+                                <i class="ico ico-chevron-down"></i>
+                            </li>
+                            <li class="left">
+                                Sports
+                                <i class="ico ico-chevron-down"></i>
+                            </li>
+                            <li class="left">
+                                Equipment
+                                <i class="ico ico-chevron-down"></i>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <h1>home page</h1>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
+        <h6>1</h6>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require '_base.php';
+
+$_title = 'Home';
+
+include '_head.php';
+?>
+
+<?php
+include '_foot.php';
