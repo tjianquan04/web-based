964d8c4cb238e4c34024cc93b075ba5737795d1e
TANJIANQUAN <tan472146@gmail.com>
2024-12-28 20:15:57 +0800

    fix some bugs and add T&C pages

--- _head.php
+++ _head.php
@@ -24,7 +24,6 @@ $dir = isset($_GET['page']) ? $_GET['page'] : 'default_page_value';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
-<div id="info"><?= temp('info') ?></div>
 <style>
 
 </style>
